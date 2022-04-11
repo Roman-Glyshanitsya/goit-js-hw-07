737a8f78075493d3b826c74eddfa018f7ad5133e
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2022-04-11 21:28:04 +0300

    hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageGalleryList = document.querySelector('.gallery');
+const galleryMarkup = createsImageGalleryMarkup(galleryItems);
+
+imageGalleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+imageGalleryList.addEventListener('click', onImageClick);
+
+function createsImageGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+       />
+      </a>
+  `;
+    })
+    .join('');
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
